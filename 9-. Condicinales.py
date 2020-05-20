33b53b668849e9836cd82b167aa98becaf577de7
Víctor Manuel Velázquez Fuentes <61021780+VictorMany@users.noreply.github.com>
2020-05-20 12:23:14 -0500

    Update 9-. Condicinales.py

--- 9-. Condicinales.py
+++ 9-. Condicinales.py
@@ -1,5 +1,5 @@
 #Condicionales
-a = int(input("Digitu un numero"))
+a = int(input("Digite un numero"))
 
 """Cuando todas las lieas estan en la identacin del if, entonces prtenecen al mismo condiconal aun"""
 if a > 0:
